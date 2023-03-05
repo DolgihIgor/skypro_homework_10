f10d704de9b6bffc852755725cdd3096d884d88d
Igor Dolgikh <87515633+DolgihIgor@users.noreply.github.com>
2023-03-05 18:21:26 +0700

    Completed work on the main page

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from flask import Flask
-
+from utils import load_candidates
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def page_index():
-    return "Наша страничка!"
+    candidates = load_candidates()
+    page_text = "<pre>"
+    for i in candidates.items():
+        page_text += f'{i[1]["name"]}\n{i[1]["position"]}\n{i[1]["skills"]}\n\n'
+    page_text += "</pre>"
+    return page_text
 
 
 app.run(host="127.0.0.1", port=8080)
