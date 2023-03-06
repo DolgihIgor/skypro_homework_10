2f8e8aa7dd5b899fc24d3a32721592390eaaa1fa
DolgihIgor <igor_dolgih@inbox.ru>
2023-03-06 09:05:22 +0700

    Added route page candidates skills

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 def page_index():
     candidates = load_candidates()
     page_text = "<pre>"
-    for candidate in candidates.items():
-        page_text += f'{candidate[1]["name"]}\n{candidate[1]["position"]}\n{candidate[1]["skills"]}\n\n'
+    for candidate in candidates.values():
+        page_text += f'{candidate["name"]}\n{candidate["position"]}\n{candidate["skills"]}\n\n'
     page_text += "</pre>"
     return page_text
 
@@ -19,6 +19,22 @@ def page_candidates(id):
     candidates = load_candidates()
     candidate = candidates[id]
     page_text = f"<pre><img src={candidate['picture']}>\n\n{candidate['name']}\n{candidate['position']}\n{candidate['skills']}</pre>"
+    return page_text
+
+
+@app.route("/skill/<skill>")
+def page_skill(skill):
+    candidates = load_candidates()
+    page_text = "<pre>\n"
+    for candidate in candidates.values():
+        candidate_skills = candidate['skills'].split(', ')
+        candidate_skills = [x.lower() for x in candidate_skills]
+        print(candidate_skills)
+        if skill in candidate_skills:
+            page_text += f"{candidate['name']}\n{candidate['position']}\n{candidate['skills']}\n\n"
+            print(candidate)
+    page_text += "</pre>"
+    print(page_text)
     return page_text
 
 
