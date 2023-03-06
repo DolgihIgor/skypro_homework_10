543bd76250588b5e224500c584dd37cfc5040b83
DolgihIgor <igor_dolgih@inbox.ru>
2023-03-06 08:23:55 +0700

    Added route page candidates

--- main.py
+++ main.py
@@ -8,10 +8,18 @@ app = Flask(__name__)
 def page_index():
     candidates = load_candidates()
     page_text = "<pre>"
-    for i in candidates.items():
-        page_text += f'{i[1]["name"]}\n{i[1]["position"]}\n{i[1]["skills"]}\n\n'
+    for candidate in candidates.items():
+        page_text += f'{candidate[1]["name"]}\n{candidate[1]["position"]}\n{candidate[1]["skills"]}\n\n'
     page_text += "</pre>"
     return page_text
 
 
-app.run(host="127.0.0.1", port=8080)
+@app.route("/candidates/<int:id>")
+def page_candidates(id):
+    candidates = load_candidates()
+    candidate = candidates[id]
+    page_text = f"<pre><img src={candidate['picture']}>\n\n{candidate['name']}\n{candidate['position']}\n{candidate['skills']}</pre>"
+    return page_text
+
+
+app.run(host="127.0.0.2", port=8080)
